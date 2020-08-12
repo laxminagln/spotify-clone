b056a83c0ba68cacf409cf183d7a30c90056facc
nagln <laxminagln@gmail.com>
2020-08-12 18:22:07 +0530

    NO

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { DataLayer } from './DataLayer';
+import { initialState } from './reducer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer
+      initialState={initialState}
+      reducer
+    >
+      <App />
+    </DataLayer>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null
+};
+
+const reducer = (state, action) => {
+
+}
+export default reducer;

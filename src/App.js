516afcc410f23be3b0b387087f9faa8d476fa17b
nagln <laxminagln@gmail.com>
2020-08-12 17:30:44 +0530

    node

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+    if(token) {
+      setToken(token);
+    }
+  }, []);
   return (
     <div className="app">
-      <Login />
+    {
+      token ? (
+        <h1>Logged in</h1>
+      ) : (
+        <Login />
+      )
+    }
     </div>
   );
 }

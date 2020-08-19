fa04b616da0203eda425224066a1ad47fb4014c2
nagln <laxminagln@gmail.com>
2020-08-19 21:30:01 +0530

    update

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer_left">
-
+3-6-21
 
             </div>
             <div className="footer_center">

490dba14d58cceb9d5dc376f5940f54b127dcc40
nagln <laxminagln@gmail.com>
2020-08-23 03:02:43 +0530

    update

--- src/Footer.js
+++ src/Footer.js
@@ -4,13 +4,16 @@ import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import RepeatIcon from "@material-ui/icons/Repeat";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import { Grid, Slider } from '@material-ui/core';
 import './Footer.css';
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer_left">
-
+3 15 00
 
             </div>
             <div className="footer_center">
@@ -19,11 +22,19 @@ function Footer() {
                 <PlayCircleOutlineIcon className="footer_icon" fontSize="large" />
                 <SkipNextIcon className="footer_icon" />
                 <RepeatIcon className="footer_green" />
-                
             </div>
             <div className="footer_right">
-
-                
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
             </div>
         </div>
     )

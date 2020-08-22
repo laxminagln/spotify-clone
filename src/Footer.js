4fbf7cef9e8fe26f4b2d75f80f86cacadfaa4e33
nagln <laxminagln@gmail.com>
2020-08-23 02:44:40 +0530

    update

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
 import './Footer.css';
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer_left">
-3-6-21
+
 
             </div>
             <div className="footer_center">
-
+                <ShuffleIcon className="footer_green" />
                 
             </div>
             <div className="footer_right">
